2142c1a89df48b3408a9560d33288572339275fd
rewfe <qrewfe@yandex.ru>
2015-05-13 05:05:26 +0300

    core data

---
+++ Tutorial_15/DeviceDetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  DeviceDetailViewController.m
+//  Tutorial_15
+//
+//  Created by Admin on 13.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "DeviceDetailViewController.h"
+
+@interface DeviceDetailViewController ()
+
+@end
+
+@implementation DeviceDetailViewController
+
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cancel:(id)sender {
+}
+
+- (IBAction)save:(id)sender {
+}
+@end
